960ac1eb49cc0e1d0014304314e005889e8ae398
Rooke0023 <rookeantonio@gmail.com>
2024-10-24 12:22:34 -0600

    Learned about packages and modules in detail while doing itunes.py

--- Input_loop.py
+++ Input_loop.py
@@ -1,24 +1,3 @@
-"""This function checks whether the input from the user is a string or not.
-
-    Returns:
-        Greetings: If the input is a string
-        Correction: Prompts the user to input a valid name.
-        
-"""
-def check():
-    
-    
-    name = input("What is your name? ")
-    while not name.isalpha():
-        print("It is not a valid name. Please try again.")
-        continue
-    else: 
-        print(f"Welcome {name}!")
-        
-        
-#Checking whether the input is really a string or not   
-
-
 #The main purpose of this function is to call two additional functions. It prompts the user for an integer and then prints "meow" a corresponding number of times.
 def main():
     number  = getnumber()
--- itunes.py
+++ itunes.py
@@ -1,5 +1,7 @@
 #CS50 Timestamp: 5:52:00
 
+
+#To understand what goes inside when I am calling a package called requests yet the syntax I use seems as if it is a module, read the document created in Notion on 24th October, 2024
 #Creating a web crawler in python that interacts with the itunes API to search for a song by the artist named Weezer.
 
 import json
@@ -11,14 +13,27 @@ import sys
 if len(sys.argv) != 2:
     sys.exit("Error occurred. Don't give too many arguments. Limit it to only one.")
 
+
+# Create a variable 'response' that refers to a Response object
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+
+# Create a variable 'jsonFile' that refers to a dictionary object
+# (created by calling the json() method on our Response object)
+jsonFile = response.json()
+
+# Now jsonFile is a variable referring to a Python dictionary object
+# containing the parsed JSON data from the response
+
 
 #This code is meant to convert a JSON response from itunes API into a formatted JSON string with indentation for readability. The .json() is a method provided by the requests library that is performed on the object variable named response.
 
 jsonFile = response.json()    #Created a varible that stores the https request in a json file
 beautifyJsonFile = json.dumps(jsonFile, indent = 2)    #Converts the json file into python readable file
-print(beautifyJsonFile)
 
-    
+print(type(requests))
+print(type(response))
+print(type(response.json())) 
+#print(dir(requests))     #Shows the amount of folder or directories inside of the requests package
+print(requests.get.__module__)   #Shows where somthing originates from
     
 
